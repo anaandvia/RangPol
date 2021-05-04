fc2a862e8e7b4d584bd91d1d64b9410d4c95d937
Ana Jannatu Uzlifat <59140031+anaandvia@users.noreply.github.com>
2021-05-04 13:40:33 +0700

    Add files via upload

---
+++ akses.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+$user = $_SESSION['username'];
+if(!isset($_SESSION['level'])){
+	echo '<script language="javascript">alert("Anda harus Login!"); document.location="admin/login.php";</script>';
+}
+
+?>
